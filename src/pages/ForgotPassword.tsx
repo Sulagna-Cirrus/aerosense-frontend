8e9f34bc68555c6f8fac667ad8bbc438c0bdd35e
Sulagna-Cirrus <sulagnar@cirruscloudsystems.com>
2025-05-12 11:22:43 +0530

    Fix API prefix in ForgotPassword.tsx

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -26,7 +26,7 @@ export default function ForgotPassword() {
 
     setIsLoading(true);
     try {
-      await api.post("/api/password-reset/forgot", { email });
+      await api.post("/password-reset/forgot", { email });
       
       toast({
         title: "OTP Sent",
